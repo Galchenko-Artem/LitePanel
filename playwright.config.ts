867f70e918428ec6e0e29f664d17fa96b97eb9a5
Artem Galchenko <artem.galchenko.88@gmail.com>
2025-05-15 22:27:44 +0300

    ci: подключен dev-сервер и ожидание в Playwright workflow исправил ошибку

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,9 +29,9 @@ export default defineConfig({
     },
   ],
 
-  webServer: {
-    command: 'npm run dev',
-    url: 'http://localhost:3000',
-    reuseExistingServer: !process.env.CI,
-  },
+  // webServer: {
+  //   command: 'npm run dev',
+  //   url: 'http://localhost:3000',
+  //   reuseExistingServer: !process.env.CI,
+  // },
 });
